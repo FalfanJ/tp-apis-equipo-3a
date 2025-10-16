6227ef90d425fb171fe6e5d1ba39555cc0f1f492
FalfanJ <falfan.jos@hotmail.com>
2025-10-15 23:58:34 -0300

    post articulo

--- WebAPI/Controllers/ArticulosController.cs
+++ WebAPI/Controllers/ArticulosController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -52,6 +53,27 @@ namespace WebAPI.Controllers
             {
                 return InternalServerError(ex);
             }
+        }
+        // POST: api/Articulos
+        public HttpResponseMessage Post([FromBody] ArticulosDto art)
+        {
+            Articulos nuevo = new Articulos();
+            ArticulosNegocio artNeg = new ArticulosNegocio();
+            nuevo.Marca = new Marcas();
+            nuevo.Categoria = new Categorias();
+
+            if (string.IsNullOrEmpty(art.Codigo))
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "Sin ingreso de codigo prodcuto.");
+
+            nuevo.Codigo = art.Codigo;
+            nuevo.Nombre = art.Nombre;
+            nuevo.Descripcion = art.Descripcion;
+            nuevo.Precio = art.Precio;
+            nuevo.Marca.Id = (int)art.IdMarca;
+            nuevo.Categoria.Id = (int)art.IdCategoria;
+
+            artNeg.Agregar(nuevo);
+            return Request.CreateResponse(HttpStatusCode.OK, "Producto agregado.");
         }
     }
 }
